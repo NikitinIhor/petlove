86de45ff62fd9111c5d5f251cd9d877f43a48895
Nikitin <nikitinigor0907@gmail.com>
2025-08-05 13:45:55 +0200

    creating structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.ico",
+  },
+  title: "PetLove — Find and Buy Pets Online",
+  description:
+    "PetLove is your trusted platform to find and buy your perfect pet with ease and care.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${manrope.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+interface HomeProps {}
+
+const Home: NextPage<HomeProps> = () => {
+  return <div className="container">Home</div>;
+};
+
+export default Home;
