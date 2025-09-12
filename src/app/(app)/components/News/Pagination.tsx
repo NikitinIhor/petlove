32dda9ec559f32a70e0a63e364aa9c6927aa1fa2
Nikitin <nikitinigor0907@gmail.com>
2025-09-12 16:41:40 +0200

    feat/pagination

--- src/app/(app)/components/News/Pagination.tsx
+++ src/app/(app)/components/News/Pagination.tsx
@@ -1,4 +1,10 @@
-import { NextPage } from "next";
+import { getPaginationRange } from "@/app/utils";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 interface PaginationProps {
   currentPage: number;
@@ -6,8 +12,94 @@ interface PaginationProps {
   onPageChange: (page: number) => void;
 }
 
-const Pagination: NextPage<PaginationProps> = () => {
-  return <div>Pagination</div>;
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps) => {
+  const firstPage = 1;
+  const previousPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+  const lastPage = totalPages;
+
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPages;
+
+  const isMobileSmall = useMediaQuery(`(max-width: 30rem)`);
+  let maxVisiblePages = isMobileSmall ? 2 : 3;
+
+  const handleClick = (page: number): void => {
+    if (page >= 1 && page <= totalPages) {
+      onPageChange(page);
+    }
+  };
+
+  return (
+    totalPages > 1 && (
+      <nav className="flex items-center justify-center gap-2 pb-[80px]">
+        <button
+          disabled={isFirstPage}
+          onClick={() => handleClick(firstPage)}
+          className="cursor-pointer w-11 h-11 flex items-center justify-center rounded-full border border-[rgba(38,38,38,0.05)] 
+          text-gray-500 disabled:opacity-40"
+        >
+          <MdKeyboardDoubleArrowLeft size={20} />
+        </button>
+
+        <button
+          disabled={isFirstPage}
+          onClick={() => handleClick(previousPage)}
+          className="cursor-pointer w-11 h-11 flex items-center justify-center rounded-full border border-[rgba(38,38,38,0.05)] 
+          text-gray-500 disabled:opacity-40"
+        >
+          <IoIosArrowBack size={24} />
+        </button>
+
+        {getPaginationRange(totalPages, currentPage, maxVisiblePages).map(
+          (page, i) =>
+            page === "..." ? (
+              <div
+                key={`dots-${i}`}
+                className="w-11 h-11 flex items-center justify-center text-gray-500"
+              >
+                …
+              </div>
+            ) : (
+              <button
+                key={page}
+                onClick={() => handleClick(page as number)}
+                className={`cursor-pointer text-[18px] w-11 h-11 flex items-center justify-center rounded-full border border-[rgba(38,38,38,0.05)] 
+                ${
+                  currentPage === page
+                    ? "bg-[#F6B83D] text-white font-bold"
+                    : "text-gray-700 hover:bg-gray-100"
+                }`}
+              >
+                {page}
+              </button>
+            )
+        )}
+
+        <button
+          disabled={isLastPage}
+          onClick={() => handleClick(nextPage)}
+          className="cursor-pointer w-11 h-11 flex items-center justify-center rounded-full border border-[rgba(38,38,38,0.05)] 
+          text-gray-500 disabled:opacity-40"
+        >
+          <IoIosArrowForward size={20} />
+        </button>
+
+        <button
+          disabled={isLastPage}
+          onClick={() => handleClick(lastPage)}
+          className="cursor-pointer w-11 h-11 flex items-center justify-center rounded-full border border-[rgba(38,38,38,0.05)] 
+          text-gray-500 disabled:opacity-40"
+        >
+          <MdKeyboardDoubleArrowRight size={24} />
+        </button>
+      </nav>
+    )
+  );
 };
 
 export default Pagination;
