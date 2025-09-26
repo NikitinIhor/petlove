2e04568d44298366d41db3ecfff108f3828cd4b7
Nikitin <nikitinigor0907@gmail.com>
2025-09-26 22:18:13 +0200

    fix/logout

--- src/app/(app)/components/Profile/UserLogoutBlock.tsx
+++ src/app/(app)/components/Profile/UserLogoutBlock.tsx
@@ -1,23 +1,23 @@
-"use client";
+// "use client";
 
-import { NextPage } from "next";
-import { useState } from "react";
+// import { NextPage } from "next";
+// import { useState } from "react";
 
-interface UserLogoutBlockProps {}
+// interface UserLogoutBlockProps {}
 
-const UserLogoutBlock: NextPage<UserLogoutBlockProps> = () => {
-  const [openModal, setOpenModal] = useState(false);
+// const UserLogoutBlock: NextPage<UserLogoutBlockProps> = () => {
+//   const [openModal, setOpenModal] = useState(false);
 
-  const handleOpenModal = () => setOpenModal(true);
-  const handleCloseModal = () => setOpenModal(false);
+//   const handleOpenModal = () => setOpenModal(true);
+//   const handleCloseModal = () => setOpenModal(false);
 
-  return (
-    <>
-      <button onClick={handleOpenModal}>Log out</button>
+//   return (
+//     <>
+//       <button onClick={handleOpenModal}>Log out</button>
 
-      {openModal && <Modal onClose={handleCloseModal}></Modal>}
-    </>
-  );
-};
+//       {openModal && <Modal onClose={handleCloseModal}></Modal>}
+//     </>
+//   );
+// };
 
-export default UserLogoutBlock;
+// export default UserLogoutBlock;
